2b25d2872cc5ed58373540b8aa46ad1839155751
hjl19910901 <18710360575@163.com>
2020-02-19 15:14:38 +0800

    人脸识别

--- Facerecognition.py
+++ Facerecognition.py
@@ -1,7 +1,7 @@
 import cv2  # opencv库
-
 # 读取图片
 image = cv2.imread('image/2.jpg')
+
 #加载人脸模型库
 face_model=cv2.CascadeClassifier('plugins/opencv/haarcascades/haarcascade_frontalcatface.xml')
 #图片进行灰度处理
@@ -11,7 +11,7 @@ faces = face_model.detectMultiScale(gray)
 #标记人脸
 for (x,y,w,h) in faces:
     #1.原始图片；2坐标点；3.矩形宽高 4.颜色值(RGB)；5.线框
-    cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),2)
+    cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
 # 显示图片窗口
 cv2.imshow('faces', image)
 # 窗口暂停
